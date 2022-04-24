80ddcc72203c29a0c76f095d8354793b24b43765
jonghan-park <jonghan.park@edu.sait.ca>
2022-04-23 21:06:00 -0600

    Update exercise

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -67,6 +67,7 @@ export default class CreateExercises extends Component{
           <div className="form-group">
             <label>Username:</label>
             <select ref="userInput"
+            required
              className="form-control"
              value={this.state.username}
              onChange={this.onChangeUsername}>
@@ -80,6 +81,34 @@ export default class CreateExercises extends Component{
                  })
                }
              </select>
+          </div>
+          <div className="form-group">
+            <label>Description:</label>
+            <input type="text"
+            required
+            className='form-control'
+            value={this.state.description}
+            onChange={this.onChangeDescription} />
+          </div>
+          <div className="form-group">
+            <label>Duration (in minutes):</label>
+            <input type="text"
+            className='form-control'
+            value={this.state.duration}
+            onChange={this.onChangeDuration} />
+          </div>
+          <div className="form-group">
+            <label>Date:</label>
+            <div>
+              <DatePicker
+              selected={this.state.date}
+              onChange={this.onChangeDate} />
+            </div>
+          </div>
+
+          <div className="form-group">
+            <input type="submit" value="Create Exercise Log"
+            className='btn btn-primary' />
           </div>
         </form>
       </div>
